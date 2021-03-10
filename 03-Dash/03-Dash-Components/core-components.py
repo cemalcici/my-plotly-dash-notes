5ae9283a106fffd67ad8b11b58efaff59ef1f69d
Cemal Cici <cicicemal@gmail.com>
2021-03-10 11:24:52 +0300

    Dash Dash Components

---
+++ 03-Dash/03-Dash-Components/core-components.py
@@ -0,0 +1,41 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+# Daha fazlası için dökümantasyona bakabilirsin:
+# https://dash.plotly.com/dash-core-components
+
+app.layout = html.Div([
+    # Açılır menü oluşturma
+    html.Div([
+        html.Label("Dropdown"),
+        dcc.Dropdown(options=[
+            {"label": "New York City", "value": "NYC"},
+            {"label": "San Francisco", "value": "SF"}
+        ],
+                    value="SF")
+    ]),
+    # Aralık çubuğu oluşturma
+    html.Div([
+        html.Label("Slider"),
+        dcc.Slider(min=-10,
+                   max=10,
+                   step=0.5,
+                   value=0,
+                   marks={i: i for i in range(-10,11, 1)})
+    ]),
+    # Radio Buton oluşturma
+    html.Div([
+        html.P(html.Label("Some Radio Items")), # TODO Buradaki sorunu araştır.
+        dcc.RadioItems(options=[
+            {"label": "New York City", "value": "NYC"},
+            {"label": "San Francisco", "value": "SF"}
+        ],
+                       value="SF")
+    ])
+])
+
+if __name__ == "__main__":
+    app.run_server()
---
+++ 03-Dash/03-Dash-Components/html-components.py
@@ -0,0 +1,21 @@
+# Dash HTML Components
+import dash
+import dash_html_components as html
+
+app = dash.Dash()
+
+# Daha fazlası için dökümantasyona bakabilirsin
+# https://dash.plotly.com/dash-html-components
+
+app.layout = html.Div(["This is the outermost div!",
+                       html.Div("This is an inner div!",
+                                style={"color": "red",
+                                       "border": "2px red solid"}),
+                       html.Div(["Another inner div"],
+                                 style={"color": "blue",
+                                        "border": "3px blue solid"})],
+                      style={"color": "green",
+                             "border": "2px green solid"})
+
+if __name__ == "__main__":
+    app.run_server()
